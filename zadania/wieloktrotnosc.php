a90ac2c633dd3f57b26d8d9d6e5d1c7180b05769
Dawidkwa77 <Daw-son2000k@wp.pl>
2025-09-26 09:18:12 +0200

    wielokrotnosc.php

---
+++ zadania/wieloktrotnosc.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <?php
+    $liczba= 77;
+    ?>
+    <br>
+    <style>
+            .tabela {
+            color: red;
+            font-size: 80px;
+            text-shadow: 5px 5px #FFF000; 
+            border-collapse: collapse;
+            border-color: green;
+            padding: 50px;
+            border-style: dashed;
+            }
+</style>
+<table>
+    <tr>
+<td> 
+    <span class="tabela">
+<?php echo $liczba*1 ?>
+        </span></td>
+<td><span class="tabela">
+     <?php echo $liczba*2 ?>
+        </span></td>
+<td><span class="tabela">
+    <?php echo $liczba*3 ?>
+        </span></td>
+<td><span class="tabela">
+    <?php echo $liczba*4 ?>
+        </span></td>
+        </tr>
+        </span>
+
+
+
+    
+    
+    
+
+
+
+            
+       
+        
+
+
+
+
+
+    
+
+
+</body>
+</html>
